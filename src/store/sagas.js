458fcd3ddf4ba32066281820dace1b72ada1652b
JackTn <347142915@qq.com>
2020-04-09 14:46:25 +0800

    'redux-saga'

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -4,9 +4,13 @@ import {GET_LIST_ITEM} from './actionTypes'
 import {handleInitListItem} from './actionCreators'
 
 function* fetchUser() {
+   try {
     const res = yield axios.get('http://localhost.charlesproxy.com:3000/list.json');
     const action = handleInitListItem(res.data)
     yield put(action);
+   } catch(e) {
+    console.log(e)
+   } 
 }
 
 function* todoListSagas() {
